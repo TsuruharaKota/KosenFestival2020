5cfef3699c9ddba55edc71723ebea542c0705f5f
TsuruharaKota <m1830@s.akashi.ac.jp>
2020-10-13 16:07:35 +0900

    fix

--- kosen_ws/src/MainRobot/include/MainRobot/catmull_rom.h
+++ kosen_ws/src/MainRobot/include/MainRobot/catmull_rom.h
@@ -11,6 +11,7 @@
 #include<fstream>
 #include<string>
 #include<iomanip>
+#include<ros/ros.h>
 
 using namespace Eigen;
 using catmull_tuple = std::tuple<float, float, float>;
@@ -18,23 +19,24 @@ using route_tuple = std::tuple<float, float, float, float>;
 using std::cout;
 using std::endl;
 template<typename T>
-class PosData3{
+class PosData4{
     public:
         T x;
         T y;
-        T z;
-        PosData3(T _x, T _y, T _z):x(_x), y(_y), z(_z){}
-        PosData3 operator+(PosData3 object){
+        T theta;
+        T speed;
+        explicit PosData4(T _x, T _y, T _theta, T _speed):x(_x), y(_y), theta(_theta), speed(_speed){}
+        PosData4 operator+(PosData4 object){
             x += object.x;
             y += object.y;
             return *this;
         }
-        PosData3 operator-(PosData3 object){
+        PosData4 operator-(PosData4 object){
             x -= object.x;
             y -= object.y;
             return *this;
         }
-        PosData3 operator*(PosData3 object){
+        PosData4 operator*(PosData4 object){
             x *= object.x;
             y *= object.y;
             return *this;
@@ -46,15 +48,11 @@ class PosData3{
 };
 class CatmullRomSpline{
     public:
-        CatmullRomSpline(std::vector<route_tuple> &&input_param, int _frequency):frequency(_frequency){
+        explicit CatmullRomSpline(std::vector<route_tuple> &&input_param, int _frequency):frequency(_frequency){
             //全ての通過点を取得
             for(auto &point : input_param){
-                //速度情報を省いてPosData3型にする
-                //-----x, y, speed, θ-----
-                PosData3<float> point3d(std::get<0>(point), std::get<1>(point), std::get<3>(point));
-                transit_point.push_back(point3d);
-                vec_speed.push_back(std::get<2>(point));
-                queue_angle.push(std::get<3>(point));
+                PosData4<float> point4d(std::get<0>(point), std::get<1>(point), std::get<2>(point), std::get<3>(point));
+                transit_point.push_back(point4d);
             }
         }
         std::vector<Vector4f> operator()(){
@@ -65,14 +63,16 @@ class CatmullRomSpline{
             float angle_prev = 0;
             float angle_sum = 0;
             for(int counter = 0; counter < transit_point.size() - 1; ++counter){
-                cal_L = 0;
                 auto NomalCal = [&](int n, float p0, float p1, float p2, float p3){
+                    //printf("NomalCal n = %d, p0 = %f, p1 = %f, p2 = %f, p3 = %f\n", n, p0, p1, p2, p3);
                     a[n] = -p0 + 3.0f * p1 - 3.0f * p2 + p3;
                     b[n] = 2.0f * p0 - 5.0f * p1 + 4.0f * p2 - p3;
                     c[n] = -p0 + p2;
                     d[n] = 2.0f * p1;
+                    //printf("NomalCal n = %d, b = %f, c = %f, d = %f\n", n, b[n], c[n], d[n]);
                 };
                 auto ExceptionCal = [&](int n, float p0, float p1, float p2){
+                    //printf("ExceptionCal n = %d, p0 = %f, p1 = %f, p2 = %f\n", n, p0, p1, p2);
                     if(counter == 0){
                         //First Curve
                         b[n] = p0 - 2.0f * p1 + p2;
@@ -84,24 +84,28 @@ class CatmullRomSpline{
                         c[n] = -p0 + p2;
                         d[n] = 2.0f * p1;
                     }
+                    //printf("ExceptionCal n = %d, b = %f, c = %f, d = %f\n", n, b[n], c[n], d[n]);
                 };
                 if(counter == 0){
-                    ExceptionCal(0, transit_point.at(counter).x, transit_point.at(counter + 1).x, transit_point.at(counter + 2).x);
-                    ExceptionCal(1, transit_point.at(counter).y, transit_point.at(counter + 1).y, transit_point.at(counter + 2).y);
-                    ExceptionCal(2, transit_point.at(counter).z, transit_point.at(counter + 1).z, transit_point.at(counter + 2).z);
+                    ExceptionCal(0, transit_point.at(counter).x,     transit_point.at(counter + 1).x,     transit_point.at(counter + 2).x);
+                    ExceptionCal(1, transit_point.at(counter).y,     transit_point.at(counter + 1).y,     transit_point.at(counter + 2).y);
+                    ExceptionCal(2, transit_point.at(counter).theta, transit_point.at(counter + 1).theta, transit_point.at(counter + 2).theta);
+                    ExceptionCal(3, transit_point.at(counter).speed, transit_point.at(counter + 1).speed, transit_point.at(counter + 2).speed);
                 }else if(counter == transit_point.size() - 2){
-                    ExceptionCal(0, transit_point.at(counter - 1).x, transit_point.at(counter).x, transit_point.at(counter + 1).x);
-                    ExceptionCal(1, transit_point.at(counter - 1).y, transit_point.at(counter).y, transit_point.at(counter + 1).y);
-                    ExceptionCal(2, transit_point.at(counter - 1).z, transit_point.at(counter).z, transit_point.at(counter + 1).z);
+                    ExceptionCal(0, transit_point.at(counter - 1).x,     transit_point.at(counter).x,     transit_point.at(counter + 1).x);
+                    ExceptionCal(1, transit_point.at(counter - 1).y,     transit_point.at(counter).y,     transit_point.at(counter + 1).y);
+                    ExceptionCal(2, transit_point.at(counter - 1).theta, transit_point.at(counter).theta, transit_point.at(counter + 1).theta);
+                    ExceptionCal(3, transit_point.at(counter - 1).speed, transit_point.at(counter).speed, transit_point.at(counter + 1).speed);
                 }else{
-                    NomalCal(0, transit_point.at(counter - 1).x, transit_point.at(counter).x, transit_point.at(counter + 1).x, transit_point.at(counter + 2).x);
-                    NomalCal(1, transit_point.at(counter - 1).y, transit_point.at(counter).y, transit_point.at(counter + 1).y, transit_point.at(counter + 2).y);
-                    NomalCal(2, transit_point.at(counter - 1).z, transit_point.at(counter).z, transit_point.at(counter + 1).z, transit_point.at(counter + 2).z);
+                    NomalCal(0, transit_point.at(counter - 1).x,     transit_point.at(counter).x,     transit_point.at(counter + 1).x,     transit_point.at(counter + 2).x);
+                    NomalCal(1, transit_point.at(counter - 1).y,     transit_point.at(counter).y,     transit_point.at(counter + 1).y,     transit_point.at(counter + 2).y);
+                    NomalCal(2, transit_point.at(counter - 1).theta, transit_point.at(counter).theta, transit_point.at(counter + 1).theta, transit_point.at(counter + 2).theta);
+                    NomalCal(3, transit_point.at(counter - 1).speed, transit_point.at(counter).speed, transit_point.at(counter + 1).speed, transit_point.at(counter + 2).speed);
                 }
                 for(int i = 0; i < frequency; ++i){
                     float t = static_cast<float>(i) / frequency;
-                    static float temp[4];
-                    for(int k = 0; k < 3; ++k){
+                    float temp[4];
+                    for(int k = 0; k < 4; ++k){
                         if(counter == 0){
                             temp[k] = 0.5f * ((b[k] * t * t) + (c[k] * t) + d[k]);
                         }else if(counter == transit_point.size() - 2){
@@ -110,55 +114,22 @@ class CatmullRomSpline{
                             temp[k] = 0.5f * ((a[k] * t * t * t) + (b[k] * t * t) + (c[k] * t) + d[k]);
                         }
                     }
-                    //区間距離を計算
-                    cal_L += std::sqrt(temp[0] * temp[0] + temp[1] * temp[1]);
-                    //x, yをそれぞれ入れる
-                    //ここでvector4fにしたい
-                    //x座標が距離でy座標が速度
-                    angle_sum += atan2(temp[1] - y_prev, temp[0] - x_prev);
-                    static double speed_amount{};
-                    if(fmod(frequency, 10) == 0){
-                        temp[3] = angle_sum / 10;
-                        if(vec_speed.size() > ((i / 10) + 1)){
-                            speed_amount = (vec_speed.at((i / 10) + 1) - vec_speed.at(i / 10)) / 10.0;
-                        }
-                        angle_prev = temp[3];  
-                        angle_count = 0;
-                        angle_sum = 0; 
-                    }else{
-                        temp[2] += speed_amount;
-                        temp[3] = angle_prev;
-                        ++angle_count;
-                    }
-                    //-----x, y, speed, θ-----
                     Vector4f Pos(temp[0], temp[1], temp[2], temp[3]);
                     goal_point.push_back(Pos);
                     x_prev = temp[0];
                     y_prev = temp[1];
                 }
-                queue_L_total.push(cal_L);
             }
             //この時点で座標は生成できている
             std::ofstream outputFile("Route.txt");
-            for(auto &point : goal_point){outputFile << std::fixed << std::setprecision(5) << point(0) << " " << point(1) << " " << point(2) << " " << point(3) * (180 / M_PI) << "\n";}
+            for(auto &point : goal_point){outputFile << std::fixed << std::setprecision(5) << point(0) << " " << point(1) << " " << point(2) << " " << point(3) << "\n";}
             outputFile.close();
             return goal_point;
-        }
-        Vector2f getSubPoint(){
-            //区間距離、角度の取得関数
-            Vector2f info(queue_L_total.front(), queue_angle.front());
-            queue_L_total.pop();
-            queue_angle.pop();
-            return info;
         }
     private:
         std::vector<float> transit_theta;
         std::vector<Vector4f> goal_point;
-        std::vector<PosData3<float>> transit_point;
-        std::queue<float> queue_L_total;
-        std::queue<float> queue_angle;
-        std::vector<float> vec_speed;
+        std::vector<PosData4<float>> transit_point;
         int frequency;
-        float a[3], b[3], c[3], d[3];
-        float cal_L;
+        float a[4], b[4], c[4], d[4];
 };
--- kosen_ws/src/MainRobot/src/main.cpp
+++ kosen_ws/src/MainRobot/src/main.cpp
@@ -8,22 +8,26 @@
 #include<tuple>
 #include<array>
 #include<utility>
-#include</usr/local/include/eigen3/Dense>
 #include<iostream>
 #include<queue>
 #include<fstream>
-#include<string>
 #include<iomanip>
+#include<chrono>
 #include<nav_msgs/Path.h>
 #include<std_msgs/String.h>
 #include<geometry_msgs/PoseStamped.h>
 #include<tf/transform_broadcaster.h>
 
+using namespace std::chrono;
 using namespace Eigen;
 using catmull_tuple = std::tuple<float, float, float>;
 using route_tuple = std::tuple<float, float, float, float>;
 using std::cout;
 using std::endl;
+
+inline double get_time_sec(void){
+    return static_cast<double>(duration_cast<nanoseconds>(steady_clock::now().time_since_epoch()).count())/1000000000;
+}
 
 enum class CoursePt{
   acrobatic,
@@ -36,16 +40,17 @@ std::vector<route_tuple> routeInit(){
   std::vector<route_tuple> point;
   switch(color){
     case CoursePt::acrobatic:
-      point.push_back(route_tuple( 0.0f, 0.0f,  1.0f,   0.0f));
-      point.push_back(route_tuple( 0.0f, 1.0f,  1.0f,  10.0f));
-      point.push_back(route_tuple( 0.0f, 2.0f,  1.0f,  20.0f));
-      point.push_back(route_tuple( 0.0f, 3.0f,  1.0f,  10.0f));
-      point.push_back(route_tuple(0.15f, 3.5f, -1.0f,   5.0f));
-      point.push_back(route_tuple( 0.5f, 3.9f, -1.0f,  10.0f));
-      point.push_back(route_tuple( 1.0f, 4.0f,  1.0f,  20.0f));
-      point.push_back(route_tuple( 2.0f, 4.0f,  1.0f,  20.0f));
-      point.push_back(route_tuple( 3.0f, 4.0f,  1.0f,  10.0f));
-      point.push_back(route_tuple( 4.0f, 4.0f,  1.0f,  0.0f));
+    //point.push_back(route_tuple(  x[m], y[m], angle[deg],speed[m/s]))
+      point.push_back(route_tuple(  0.0f, 0.0f,  0.0f,   0.0f));
+      point.push_back(route_tuple(  0.0f, 1.0f,  1.0f,   1.0f));
+      point.push_back(route_tuple(  0.0f, 2.0f,  2.0f,   2.0f));
+      point.push_back(route_tuple(  0.0f, 3.0f,  3.0f,   2.0f));
+      point.push_back(route_tuple( 0.15f, 3.5f,  3.0f,   2.0f));
+      point.push_back(route_tuple(  0.5f, 3.9f,  3.0f,   2.0f));
+      point.push_back(route_tuple(  1.0f, 4.0f,  3.0f,   2.0f));
+      point.push_back(route_tuple(  2.0f, 4.0f,  2.0f,   2.0f));
+      point.push_back(route_tuple(  3.0f, 4.0f,  1.0f,   1.0f));
+      point.push_back(route_tuple(  4.0f, 4.0f,  0.0f,   0.0f));
       break;
     case CoursePt::qualifying:
       break;
@@ -55,14 +60,14 @@ std::vector<route_tuple> routeInit(){
   return point;
 }
 
-bool update(double ref_time, double timer_counter){
-    static double elapsed_time_prev{};
-    double elapsed_time = (timer_counter * 0.01) - elapsed_time_prev;
+bool update(double ref_time, double timer){
+    //elapsed_time_prevは初期化しないといけない(微小の誤差が出る)
+    static double time_prev{};
+    double elapsed_time = timer - time_prev;
     if(ref_time < elapsed_time){
-        elapsed_time_prev = elapsed_time;
+        time_prev = timer;
         return true;
     }else{
-        //ROS_INFO("%lf %lf", ref_time, elapsed_time);
         return false;
     }
 }
@@ -74,32 +79,37 @@ int main(int argc, char **argv){
     std::string target_frame = "unit0_link";
        
     CatmullRomSpline splineObject(routeInit<CoursePt::acrobatic>(), 100);
+    //経路生成終了
     std::vector<Vector4f> route_info = splineObject();
 
     //0.01秒ごとに更新される
     ros::Rate loop_rate(100);
     int route_counter{};
-    int loop_counter{};
-    double ref_x = 0, ref_y = 0, ref_rad = 0, ref_speed = 0, ref_time = 0;
+    unsigned long int loop_counter{};
+    double ref_x = 0, ref_x_diff = 0, ref_x_prev = 0, ref_y = 0, ref_y_diff = 0, ref_y_prev = 0, ref_rad = 0, ref_speed = 0, ref_time = 0;
+   
     while(ros::ok()){
         ++loop_counter;
-        if(update(ref_time, loop_counter)){
+        if(update(ref_time, loop_counter * 0.01)){
             ++route_counter;
+            if(route_info.size() < route_counter){return 0;}
             Vector4f nowParam = route_info.at(route_counter);
             ref_x = nowParam(0);
             ref_y = nowParam(1);
-            ref_speed = nowParam(2);
-            ref_rad = nowParam(3);
-            ref_time = ((ref_x * ref_x) + (ref_y * ref_y)) / ref_speed;
-            //ROS_INFO("ref_time = %lf\n", ref_speed);
-            ROS_INFO("%lf %lf\n", ref_x, ref_y);
-            //ROS_INFO("%lf %lf %lf %lf %lf\n", ref_x, ref_y, ref_rad, ref_speed, ref_time);
+            ref_x_diff = ref_x - ref_x_prev;
+            ref_y_diff = ref_y - ref_y_prev;
+            ref_rad = nowParam(2);
+            ref_speed = nowParam(3);
+            ref_time = std::sqrt(((ref_x_diff * ref_x_diff) + (ref_y_diff * ref_y_diff))) / ref_speed;
+            ROS_INFO("%lf %lf %lf %lf\n", ref_x, ref_y, ref_speed, ref_time);
+            ref_x_prev = ref_x;
+            ref_y_prev = ref_y;
         }
         
         geometry_msgs::Pose t_pose;
 
-        t_pose.position.x = ref_x / 10;
-        t_pose.position.y = ref_y / 10;
+        t_pose.position.x = ref_x;
+        t_pose.position.y = ref_y;
         t_pose.orientation.w = 1.0;
         static tf::TransformBroadcaster br;
         tf::Transform transform;
