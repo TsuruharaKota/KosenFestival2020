1d336dc115f16324343702bbac3b3a23fc0e2b44
TsuruharaKota <m1830@s.akashi.ac.jp>
2020-10-29 19:39:35 +0900

    fix

--- stm_system/src/main.cpp
+++ stm_system/src/main.cpp
@@ -2,9 +2,9 @@
 //#include"amt_encoder.h"
 //#include"motor_driver.h"
 #include"serial.h"
-
+/*
 int main(){
-/*  Enc motor_enc[4] = {Enc(D14, D15), Enc(D13, D12), Enc(D11, D10), Enc(D18, D19)};
+  Enc motor_enc[4] = {Enc(D14, D15), Enc(D13, D12), Enc(D11, D10), Enc(D18, D19)};
   Enc  odom_enc[4] = {Enc(D14, D15), Enc(D13, D12), Enc(D11, D10), Enc(D18, D19)};
   MD motor[4] = {MD(), MD(), MD(), MD()};
   MySerial pc(PB_8, PB_9);
@@ -17,4 +17,17 @@ int main(){
       motor.output(ref_speed[i], motor_enc[i].getSpeed());
     }
   }
-*/}
+}*/
+
+Serial pc(USBTX, USBRX, 9600); // tx, rx
+
+int main() {
+    //float send_data[9] = {};
+    float receive_data[4]{};
+    int counter{};
+    while(1) {
+        //++counter;
+        //float send_data[9];
+        serialReceive(receive_data, pc);
+    }
+}
--- stm_system/src/serial.h
+++ stm_system/src/serial.h
@@ -2,44 +2,58 @@
 #define SERIAL_H_
 
 #include"mbed.h"
+#include"string.h"
+
 #define HEAD_BYTE 0xFF
 #define STX 0x02
-DigitalOut led(LED1);
-void serialSend(float send_data, Serial &obj){ 
+void serialSend(float *send_data, Serial &obj){ 
   uint8_t checksum_send = 0;
-  unsigned char data_h_h{};
-  unsigned char data_h_l{};
-  unsigned char data_l_h{};
-  unsigned char data_l_l{};
-  uint32_t byte_divide{};
-  memcpy(&byte_divide, &send_data, 4);
-  data_h_h = (byte_divide >> 24) & 0xFF;
-  data_h_l = (byte_divide >> 16) & 0xFF;
-  data_l_h = (byte_divide >> 8) & 0xFF;
-  data_l_l = (byte_divide >> 0) & 0xFF;
-  unsigned char sendFormat[5] = {0, data_h_h, data_h_l, data_l_h, data_l_l};
+  unsigned char data_h_h[9]{};
+  unsigned char data_h_l[9]{};
+  unsigned char data_l_h[9]{};
+  unsigned char data_l_l[9]{};
+  uint32_t byte_divide[9]{};
+  for(int i = 0; i < 9; ++i){
+    memcpy(&byte_divide[i], &send_data[i], 4);
+    data_h_h[i] = (byte_divide[i] >> 24) & 0xFF;
+    data_h_l[i] = (byte_divide[i] >> 16) & 0xFF;
+    data_l_h[i] = (byte_divide[i] >> 8) & 0xFF;
+    data_l_l[i] = (byte_divide[i] >> 0) & 0xFF;
+  }
+  unsigned char sendFormat[9][5] = {
+    {0, data_h_h[0], data_h_l[0], data_l_h[0], data_l_l[0]},
+    {1, data_h_h[1], data_h_l[1], data_l_h[1], data_l_l[1]},
+    {2, data_h_h[2], data_h_l[2], data_l_h[2], data_l_l[2]},
+    {3, data_h_h[3], data_h_l[3], data_l_h[3], data_l_l[3]},
+    {4, data_h_h[4], data_h_l[4], data_l_h[4], data_l_l[4]},
+    {5, data_h_h[5], data_h_l[5], data_l_h[5], data_l_l[5]},
+    {6, data_h_h[6], data_h_l[6], data_l_h[6], data_l_l[6]},
+    {7, data_h_h[7], data_h_l[7], data_l_h[7], data_l_l[7]},
+    {8, data_h_h[8], data_h_l[8], data_l_h[8], data_l_l[8]},
+  };
   //send head byte
   obj.putc(HEAD_BYTE);
   checksum_send += HEAD_BYTE;
   obj.putc(STX);
   checksum_send += STX;
-  for(int k = 0; k < 5; ++k){
-    obj.putc(sendFormat[k]);
-    checksum_send += sendFormat[k];
+  for(int i = 0; i < 9; ++i){
+    for(int k = 0; k < 5; ++k){
+      obj.putc(sendFormat[i][k]);
+      checksum_send += sendFormat[i][k];
+    }
   }
   //send checksum
   obj.putc(checksum_send);
 }
 void serialReceive(float *receive_result, Serial &obj){
-  led = 1;
   uint8_t got_data{};
   uint8_t checksum_receive{};
-  uint8_t receive_data[5];
+  uint8_t receive_data[4];
   unsigned char receiveFormat[4][5] = {
     {0, 0, 0, 0, 0},
     {1, 0, 0, 0, 0},
     {2, 0, 0, 0, 0},
-    {3, 0, 0, 0, 0}
+    {3, 0, 0, 0, 0},
   };
   got_data = static_cast<uint8_t>(obj.getc());
   if(got_data == HEAD_BYTE){
@@ -51,7 +65,10 @@ void serialReceive(float *receive_result, Serial &obj){
         for(int i = 0; i < 5; ++i){
           receive_data[i] = static_cast<uint8_t>(obj.getc());
           checksum_receive += receive_data[i];
-          receiveFormat[receive_data[0]][i] = receive_data[i];
+          int temp_data  = static_cast<int>(receive_data[0]);
+          if((0 <= temp_data) && (temp_data <= 4)){
+            receiveFormat[receive_data[0]][i] = receive_data[i];
+          }
         }
       }
       got_data = static_cast<uint8_t>(obj.getc());
@@ -60,9 +77,9 @@ void serialReceive(float *receive_result, Serial &obj){
         for(int i = 0; i < 4; ++i){
           //receiveFormat[i][0]はidである
           result[i] = static_cast<int32_t>((receiveFormat[i][1] << 24 & 0xFF000000)
-                                           | (receiveFormat[i][2] << 16 & 0x00FF0000)
-                                           | (receiveFormat[i][3] <<  8 & 0x0000FF00)
-                                           | (receiveFormat[i][4] <<  0 & 0x000000FF)
+                                         | (receiveFormat[i][2] << 16 & 0x00FF0000)
+                                         | (receiveFormat[i][3] <<  8 & 0x0000FF00)
+                                         | (receiveFormat[i][4] <<  0 & 0x000000FF)
           );
           memcpy(&receive_result[i], &result[i], 4);
         }
@@ -70,5 +87,4 @@ void serialReceive(float *receive_result, Serial &obj){
     }
   }
 }
-
 #endif
